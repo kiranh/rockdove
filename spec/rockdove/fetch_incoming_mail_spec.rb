20c5d362292df2bf46f58fdbc3ffc940f78ee367
Kiran Soumya <kiran.soumya@gmail.com>
2012-06-27 12:33:36 +0530

    got the tests working wihout making the actual connection

--- spec/rockdove/fetch_incoming_mail_spec.rb
+++ spec/rockdove/fetch_incoming_mail_spec.rb
@@ -2,20 +2,28 @@ require 'spec_helper.rb'
 
 describe "FetchIncomingMail" do
 
+  module Rockdove
+    class EWS
+      attr_accessor :to_recipients, :date_time_created, :date_time_sent, :from
+      attr_accessor :subject, :body, :body_type, :attachments
+    end
+  end
+
   before(:each) do
-    Rockdove::Follow::Ready.stub!(:connect).and_return(:true)
     @mail_retriever = Rockdove::Follow::Action.new()
+    @connection = Rockdove::Follow::Ready
   end
   
   it "should validate the exchange server info provided" do
-    Rockdove::Follow::Ready.url.should_not be_nil
-    Rockdove::Follow::Ready.username.should_not be_nil
-    Rockdove::Follow::Ready.password.should_not be_nil
-    Rockdove::Follow::Ready.incoming_folder.should_not be_nil    
+    @connection.url.should_not be_nil
+    @connection.username.should_not be_nil
+    @connection.password.should_not be_nil
+    @connection.incoming_folder.should_not be_nil    
   end
 
   it "should connect to the Exchange Server" do
-    Rockdove::Follow::Ready.connect.should == :true
+    @connection.should_receive(:connect).and_return(true)
+    @connection.connect.should == true
   end
 
   it "is trying to fetch mail when there is no incoming mail" do
@@ -25,31 +33,33 @@ describe "FetchIncomingMail" do
 
   it "is trying to fetch a new forwarded mail" do
     mail = get_mail("forwarded_mail")
-    @mail_retriever.should_receive(:fetch_from_box).and_return(mail)       
-    @mail_retriever.retrieve_mail.should be_an_instance_of(Rockdove::ExchangeMail)
-    @mail_retriever.should_receive(:fetch_from_box).and_return(mail)       
+    test_instance_of(mail)
+    stub_fetch_from_box(mail)       
     @mail_retriever.retrieve_mail.body.should == "FYI"    
   end
 
   it "is trying to fetch a new mail" do
     mail = get_mail("new_mail")
-    @mail_retriever.should_receive(:fetch_from_box).and_return(mail)       
-    @mail_retriever.retrieve_mail.should be_an_instance_of(Rockdove::ExchangeMail)
-    @mail_retriever.should_receive(:fetch_from_box).and_return(mail)       
+    test_instance_of(mail)
+    stub_fetch_from_box(mail)      
     @mail_retriever.retrieve_mail.body.should == "Hi, This is a new post"    
   end
 
   it "is trying to fetch a new replied mail" do
     mail = get_mail("replied_mail")
-    @mail_retriever.should_receive(:fetch_from_box).and_return(mail)       
-    @mail_retriever.retrieve_mail.should be_an_instance_of(Rockdove::ExchangeMail)
-    @mail_retriever.should_receive(:fetch_from_box).and_return(mail)       
+    test_instance_of(mail)
+    stub_fetch_from_box(mail)     
     @mail_retriever.retrieve_mail.body.should == "This is a replied post from outlook"    
   end
 
-  #it "should connect to the Exchange Server" do
-  #  puts Rockdove::Follow::Action.new().retrieve_mail.body.inspect
-  #end
+  def test_instance_of(mail)
+    stub_fetch_from_box(mail)     
+    @mail_retriever.retrieve_mail.should be_an_instance_of(Rockdove::ExchangeMail)
+  end
+
+  def stub_fetch_from_box(mail)
+    @mail_retriever.should_receive(:fetch_from_box).and_return(mail)
+  end
 
   def get_mail(name)
     YAML.load(email(name.to_sym))    
