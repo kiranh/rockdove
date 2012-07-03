76ca471557d72693de32296eaa7a15c99cd1461a
Kiran Soumya <kiran.soumya@gmail.com>
2012-07-03 18:10:14 +0530

    purged this file and got back

---
+++ lib/rockdove/dove_parser.rb
@@ -0,0 +1,32 @@
+require 'email_reply_parser'
+module Rockdove
+  class DoveParser
+    HTML_REGEXP = /<\/?[^>]*>/
+    FWD_REGEXP = /^(On(.+)wrote:.+\z)$/nm
+    SIGNATURE_REGEXP = /^(Thanks(.+)Regards.+\z)$/inm
+    REPLY_REGEXP = /^(From:(.+)Sent:.+\z)$/nm
+    DASHES = "________________________________________"
+
+    def parse_mail(mail, body_type)
+      Rockdove.logger.info "Rockdove is parsing the mail content now..."
+      return nil unless mail
+      mail.gsub!(HTML_REGEXP, "").strip! if body_type == "HTML"
+      parsed_content = EmailReplyParser.parse_reply(mail) 
+      choose_and_parse(parsed_content)
+    end
+
+    def choose_and_parse(content)
+      content.gsub!(DASHES,"")      
+      case content
+        when REPLY_REGEXP
+          content.gsub(REPLY_REGEXP,"").strip!
+        when FWD_REGEXP
+          content.gsub(FWD_REGEXP,"").strip!
+        when SIGNATURE_REGEXP
+          content.gsub(SIGNATURE_REGEXP,"").strip! 
+        else
+          content
+      end
+    end   
+  end
+end
